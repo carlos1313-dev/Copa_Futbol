bf9391bdc95871f003a1c3ff2c5fc7fabcf5b20c
Carlos E <sangroniscarlosenrique1@gmail.com>
2025-08-04 00:08:52 -0500

    Añadi parametro de busqueda en country: mundialista

--- copa_teams/src/main/java/com/pes/copa/teamscontroller/CountryController.java
+++ copa_teams/src/main/java/com/pes/copa/teamscontroller/CountryController.java
@@ -4,10 +4,31 @@
  */
 package com.pes.copa.teamscontroller;
 
+import com.pes.copa.teams.entity.Country;
+import com.pes.copa.teams.service.CountryService;
+import com.pes.copa.teams.service.TeamsService;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author sangr
  */
+@RestController
 public class CountryController {
+    private CountryService countryService;
+    
+    @GetMapping("country/continent/{continent}")
+    public List<Country> getCountriesByContinent(@PathVariable String continent){
+        return countryService.getCountriesByContinent(continent);
+    }
+    
+    @GetMapping("country/mundialist")
+    public List<Country> getCountriesByIsMundialist(){
+        return countryService.getCountriesByIsMundialist();
+    }
+    
     
 }
